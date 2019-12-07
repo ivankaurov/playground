b119c563772046692d56abb64c296e289b1cd5da
Ivan Kaurov <ivan.kaurov@gmail.com>
2019-12-07 16:32:39 +0300

    Solution file

---
+++ src/Shared/AssemblyInfo.Shared.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests.Unit")]
+[assembly: InternalsVisibleTo("Tests.Integration")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
