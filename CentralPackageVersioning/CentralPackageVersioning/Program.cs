c00ab44f732cadee11b6760267825fefa52f46b3
Ivan Kaurov <ivan.kaurov@sievo.com>
2021-02-22 11:47:59 +0300

    CPV PoC

---
+++ CentralPackageVersioning/CentralPackageVersioning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CentralPackageVersioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! Newtonsoft is {typeof(JsonSerializer).Assembly.GetName().Version}");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ CentralPackageVersioning/LegacyCsproj/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LegacyCsproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! Newtonsoft is {typeof(JsonSerializer).Assembly.GetName().Version}");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
